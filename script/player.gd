8064a61e31a02695bcdd710aa9f203b698a00b8a
Alfred Liusten <alfredliu.development@gmail.com>
2025-12-05 18:20:06 +0700

    First Game part 1

--- script/player.gd
+++ script/player.gd
@@ -4,6 +4,11 @@ const SPEED = 400.0
 const JUMP_VELOCITY = -800.0
 @onready var player_animation: AnimatedSprite2D = $AnimatedSprite2D
 
+var initial_y_position: float
+var max_reached_y_position: float
+var many_height = 0
+#@onready var height: Label = $"../CanvasLayer/MarginContainer/Height"
+
 func _physics_process(delta: float) -> void:
 	#print(velocity)
 	#print(delta)
@@ -20,22 +25,27 @@ func _physics_process(delta: float) -> void:
 		#print(velocity)
 		if velocity.y < 0:
 			player_animation.animation = "jump"
+			many_height += 1
 		
 		elif velocity.y > 0:
 			player_animation.animation = "fall"
+			if many_height > 0:
+				many_height -= 1
+	
+	print(many_height)
 	
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump_2D") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("left_2D", "right_2D")
 	if direction:
 		player_animation.flip_h = direction < 0
 		
 		velocity.x = direction * SPEED
-
+	
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
